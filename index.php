bf7e2a356fddbd4af7527e196d1fd1a26d5acde0
Ma4kaigrisho <ivanovnikola0021@gmail.com>
2023-08-20 11:18:33 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+<body>
+    <a href="logout.php">logout</a>
+    <h1>This is the index page</h1>
+</body>
+</html>
